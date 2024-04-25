2d21ac345c4e0aed31288903d1174909a09ca7b8
notCroptu <martinsmariana1972@gmail.com>
2024-04-25 19:31:21 +0100

    Ex6 - edit Player class

---
+++ PlayerManager4/CompareByName.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PlayerManager4
+{
+    public class CompareByName : IComparer<Player>
+    {
+        private bool order;
+        public CompareByName(bool order = true)
+        {
+            this.order = order;
+        }
+
+        public int Compare(Player player1, Player player2)
+        {
+            int result = string.Compare(player1.Name, player2.Name);
+            return order ? result : - result;
+        }
+    }
+}
---
+++ PlayerManager4/Player.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PlayerManager4
+{
+    public class Player : IComparable<Player>
+    {
+        public string Name { get; }
+        public int Score { get; set; }
+        public Player(string name, int score)
+        {
+            Name = name;
+            Score = score;
+        }
+
+        public int CompareTo(Player other)
+        {
+            if (other == null) return 1;
+            return other.Score - this.Score;
+        }
+    }
+}
